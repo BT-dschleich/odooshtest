9a232ad68a787dfab1612988b36c0de0e2236f06
Vagrant Default User <openerp@odoo-dev>
2017-10-16 10:38:26 +0200

    test2fix

--- blibla/myaddons/testituut/models/models.py
+++ blibla/myaddons/testituut/models/models.py
@@ -5,7 +5,7 @@ import odoo
 import logging
 _logger = logging.getLogger(__name__)
 addonspath = odoo.tools.config['addons_path']
-logger.debug('DEBUG TESTITUUUUT ADDONSPATH: %s' % addonspath)
+_logger.debug('DEBUG TESTITUUUUT ADDONSPATH: %s' % addonspath)
 
 # class testit(models.Model):
 
